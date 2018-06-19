127f42bbf19ba31e0dff1a95cb34e8fce2d73d77
cryptozeny <cryptozeny@gmail.com>
2018-06-20 01:09:07 +0900

    add: __ARM_NEON__

--- yescrypt/yescrypt-best.c
+++ yescrypt/yescrypt-best.c
@@ -1,4 +1,16 @@
-#ifdef __SSE2__
+// 원본
+// #ifdef __SSE2__
+// #include "yescrypt-simd.c"
+// #else
+// #include "yescrypt-opt.c"
+// #endif
+
+
+
+// 테스트
+#if defined(__ARM_NEON__)||defined(__ARM_NEON)
+#include "yescrypt-neon.c"
+#elif defined __SSE2__
 #include "yescrypt-simd.c"
 #else
 #include "yescrypt-opt.c"
